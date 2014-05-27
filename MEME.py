5c33d2b352cdb80147011ef6768e8c817555c060
tchnl <tch@live.nl>
2014-05-27 22:13:43 +0200

    new meme

--- MEME.py
+++ MEME.py
@@ -9,7 +9,7 @@ def main():
 ##        MEME_input = "ftp://thierry@cytosine.nl/home/thierry/c11_pipeline/genes"+str(i)+".fa"
 ##        subprocess.call("cd ftp://thierry@cytosine.nl/sharing/students/MEME/bin ; ./meme <"+str(MEME_input)+"> -dna -oc . -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -revcomp -o ftp://thierry@cytosine.nl/home/thierry/c11_pipeline/", shell=True)
     
-    MEME_input = "cytosine.nl/home/thierry/c11_pipeline/genes0.fa"
-    subprocess.call("cd cytosine.nl/sharing/students/MEME/bin/ ; ./meme <"+str(MEME_input)+"> -dna -oc . -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -revcomp -o ftp://thierry@cytosine.nl/home/thierry/c11_pipeline/", shell=True)
+    MEME_input = "/home/thierry/c11_pipeline/genes0.fa"
+    subprocess.call("cd /sharing/students/MEME/bin/ ; ./meme <"+str(MEME_input)+"> -dna -oc . -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -revcomp -o /home/thierry/c11_pipeline/", shell=True)
 
 main()
