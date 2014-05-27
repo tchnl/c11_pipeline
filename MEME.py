eec17b75ff9abf595662af83ffa487d8cadf28fa
tchnl <tch@live.nl>
2014-05-27 15:18:28 +0200

    newer meme

--- MEME.py
+++ MEME.py
@@ -6,7 +6,7 @@ import subprocess
 
 def main():
     for i in range(4):
-        MEME_input = "ftp://cytosine.nl/home/thierry/c11_pipeline/genes"+str(i)+".fa"
+        MEME_input = "genes"+str(i)+".fa"
         #print MEME_input
         subprocess.call("cd ftp://cytosine.nl/sharing/students/MEME/bin/ ; ./meme <"+str(MEME_input)+"> -dna -oc . -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -revcomp -o ftp://thierry@cytosine.nl/home/thierry/c11_pipeline/", shell=True)
 
