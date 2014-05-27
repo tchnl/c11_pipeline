883279b8516635f987e8205468f97782a0be7759
tchnl <tch@live.nl>
2014-05-27 14:52:10 +0200

    added meme python script

---
+++ MEME.py
@@ -0,0 +1,13 @@
+###############################################
+# MEME module for group 1, course 11 pipeline #
+###############################################
+
+import subprocess
+
+def main():
+    for i in range(4):
+        MEME_input = "ftp://thierry@cytosine.nl/home/thierry/c11_pipeline/genes"+str(i)+".fa"
+        #print MEME_input
+        subprocess.call("meme <"+str(MEME_input)+"> -dna -oc . -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -revcomp -o ftp://thierry@cytosine.nl/home/thierry/c11_pipeline/", shell=True)
+
+main()
