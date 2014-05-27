a0baf5fc3039beee0ee033d7810c4db241dd6106
tchnl <tch@live.nl>
2014-05-27 15:41:38 +0200

    newer meme

--- MEME.py
+++ MEME.py
@@ -5,9 +5,11 @@
 import subprocess
 
 def main():
-    for i in range(4):
-        MEME_input = "genes"+str(i)+".fa"
-        #print MEME_input
-        subprocess.call("./ftp://thierry@cytosine.nl/sharing/students/MEME/bin/meme <"+str(MEME_input)+"> -dna -oc . -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -revcomp -o ftp://thierry@cytosine.nl/home/thierry/c11_pipeline/", shell=True)
+##    for i in range(4):
+##        MEME_input = "ftp://thierry@cytosine.nl/home/thierry/c11_pipeline/genes"+str(i)+".fa"
+##        subprocess.call("cd ftp://thierry@cytosine.nl/sharing/students/MEME/bin ; ./meme <"+str(MEME_input)+"> -dna -oc . -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -revcomp -o ftp://thierry@cytosine.nl/home/thierry/c11_pipeline/", shell=True)
+    
+    MEME_input = "cytosine.nl/home/thierry/c11_pipeline/genes0.fa"
+    subprocess.call("cd cytosine.nl/sharing/students/MEME/bin/ ; ./meme <"+str(MEME_input)+"> -dna -oc . -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 6 -maxw 50 -revcomp -o ftp://thierry@cytosine.nl/home/thierry/c11_pipeline/", shell=True)
 
 main()
