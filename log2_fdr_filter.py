b25e625790f73e48a7e4e07b29f349617ad96ad9
tchnl <tch@live.nl>
2014-05-27 23:24:32 +0200

    new meme

---
+++ log2_fdr_filter.py
@@ -0,0 +1,17 @@
+###############################################
+# Filter log2/fdr module                      #
+###############################################
+
+def main():
+    input_data = open("/home/thierry/c11_pipeline/LP_DEG_glc.txt", "r").readlines()
+    sorted_list = []
+
+    for line in input_data[2:]:
+        line_split = line.strip().split()
+        if float(line_split[1]) > -1 and float(line_split[1]) < 1:
+            if float(line_split[2]) < 0.05:
+                sorted_list.append(line)
+
+    return sorted_list
+
+main()
