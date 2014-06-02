6b6473940fdaf9fc66f9b4d04e1f6de05be6b52e
tchnl <tch@live.nl>
2014-06-02 17:12:08 +0200

    final meme

--- MEME.py
+++ MEME.py
@@ -7,7 +7,7 @@ import subprocess
 def main():
     for i in range(4):
         MEME_input = "/home/thierry/c11_pipeline/genes"+str(i)+".fa"
-        subprocess.call("cd /sharing/students/meme/bin/ ; ./meme "+str(MEME_input)+" -oc /home/thierry/c11_pipeline/output/ -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 35 -maxw 45 ", shell=True)    
+        subprocess.call("cd /sharing/students/meme/bin/ ; ./meme "+str(MEME_input)+" -oc /home/thierry/c11_pipeline/output"+str(i)+"/ -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 35 -maxw 45 ", shell=True)    
 
 ##    MEME_input = "/home/thierry/c11_pipeline/genes0.fa"
 ##    subprocess.call("cd /sharing/students/meme/bin/ ; ./meme /home/thierry/c11_pipeline/genes0.fa -oc /home/thierry/output/ -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 35 -maxw 45 ", shell=True)
