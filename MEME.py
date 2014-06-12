f7ca363751ec6b5bc56dffb49c41ad8af3629bdf
Tommyboy14 <tomlinssen1@hotmail.com>
2014-06-12 14:00:45 +0200

    Superscript

--- MEME.py
+++ MEME.py
@@ -12,4 +12,3 @@ def main():
 ##    MEME_input = "/home/thierry/c11_pipeline/genes0.fa"
 ##    subprocess.call("cd /sharing/students/meme/bin/ ; ./meme /home/thierry/c11_pipeline/genes0.fa -oc /home/thierry/output/ -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 35 -maxw 45 ", shell=True)
 
-main()
--- superscript.py
+++ superscript.py
@@ -5,7 +5,8 @@
 
 import log2_fdr_filter
 import SortRatio
-#import convert
+import convert
+import FastaDevelop
 
 def main():
         defaultFilterFile = "LP_DEG_glc.txt"
@@ -13,7 +14,17 @@ def main():
         defaultSortFile = "LP_DEG_glc_filtered.txt"
         sort(defaultSortFile)
         defaultConvertFile = "LP_genes.txt"
-        convert(defaultConvertFile)
+        converting(defaultConvertFile)
+        defaultFastaFile1 = "LP_DEG_glc_filtered.txt"
+        defaultFastaFile2 = "LP_genes_NEW.txt"
+        defaultFastaFile3 = "WCFS1_glc_1_tss.fa"
+        defaultFastaFile4 = "WCFS1_glc_2_tss.fa"
+        defaultFastaFile5 = "NC8_glc_1_tss.fa"
+        defaultFastaFile6 = "NC8_glc_2_tss.fa"
+        fasta(defaultFastaFile1,defaultFastaFile2,defaultFastaFile3,defaultFastaFile4,defaultFastaFile5,defaultFastaFile6)
+        defaultMEMEFile1 = "genes"
+        
+        
 	
 	
 def filter (defaultFilterFile):
@@ -22,12 +33,12 @@ def filter (defaultFilterFile):
 def sort (defaultSortFile):
         x = SortRatio.main(defaultSortFile)
         print (x)
-def convert (defaultConvertFile):
+def converting (defaultConvertFile):
         x = convert.main(defaultConvertFile)
         print (x)
-def fasta():
-	print ("fasta")
-	
+def fasta(file1,file2,file3,file4,file5,file6):
+        x = FastaDevelop.main(file1,file2,file3,file4,file5,file6)
+        print (x)
 def meme():
 	print ("meme")
 
