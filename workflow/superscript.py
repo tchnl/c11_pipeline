fe244020784a034a08f09ad0a2b6fadd1b2387aa
Tommyboy14 <tomlinssen1@hotmail.com>
2014-06-20 14:22:22 +0200

    Superscript

--- workflow/superscript.py
+++ workflow/superscript.py
@@ -12,36 +12,87 @@ import MEME
 
 #1: alle zonder sort
 #2: met sort alles 
-#4:	filter
+#4: filter
 #5: sort ration
-#6:convert
+#6: convert
 #7: fasta 
 #8: meme
 #list einde list
 
 def main():
-		#getDataGUI()
+        #input = BeginGUI.main()
+        input = ["string1","string2","string3","string3","string3","string3","7"]
+        getDataGUI(input)
+
+
         defaultFilterFile = str ("LP_DEG_glc.txt")
-        filter(defaultFilterFile)
+        #filter(defaultFilterFile)
         defaultSortFile = "LP_DEG_glc_filtered.txt"
-        sort(defaultSortFile)
+        #sort(defaultSortFile)
         defaultConvertFile = "LP_genes.txt"
-        converting(defaultConvertFile)
+        #converting(defaultConvertFile)
         defaultFastaFile1 = "LP_DEG_glc_filtered.txt"
         defaultFastaFile2 = "LP_genes_NEW.txt"
         defaultFastaFile3 = "WCFS1_glc_1_tss.fa"
         defaultFastaFile4 = "WCFS1_glc_2_tss.fa"
         defaultFastaFile5 = "NC8_glc_1_tss.fa"
         defaultFastaFile6 = "NC8_glc_2_tss.fa"
-        fasta(defaultFastaFile1,defaultFastaFile2,defaultFastaFile3,defaultFastaFile4,defaultFastaFile5,defaultFastaFile6)
+        #fasta(defaultFastaFile1,defaultFastaFile2,defaultFastaFile3,defaultFastaFile4,defaultFastaFile5,defaultFastaFile6)
         defaultMEMEFile = "genes"
         #meme(defaultMEMEFile)
 
 
-def getDataGUI():
-	x = beginGUI.main()
-	
-		
+def getDataGUI(input):
+        index = int(input[-1])
+        if index == 1:
+                print ("All files are created, without the sort algorithm")
+        if index == 2:
+                print("All files are created, with the sort algorithm")
+        if index == 4:
+                file = input[0]
+                file = "LP_DEG_glc.txt"
+                defaultFilterFile = str (file)
+                filter(defaultFilterFile)
+                print ("File LP_DEG_glc_filtered.txt created")
+        if index == 5:
+                file = input[0]
+                file = "LP_DEG_glc_filtered.txt"
+                defaultSortFile = str (file)
+                sort(defaultSortFile)
+                print ("File LP_DEG_glc_filtered.txt created")
+                
+        if index == 6:
+                file = input[0]
+                file = "LP_genes.txt"
+                defaultConvertFile = str (file)
+                converting(defaultConvertFile)
+                print ("File LP_genes_NEW.txt created")
+                
+        if index == 7:
+                defaultFastaFile1 = input[0]
+                defaultFastaFile2 = input[1]
+                defaultFastaFile3 = input[2]
+                defaultFastaFile4 = input[3]
+                defaultFastaFile5 = input[4]
+                defaultFastaFile6 = input[5]
+                defaultFastaFile1 = "LP_DEG_glc_filtered.txt"
+                defaultFastaFile2 = "LP_genes_NEW.txt"
+                defaultFastaFile3 = "WCFS1_glc_1_tss.fa"
+                defaultFastaFile4 = "WCFS1_glc_2_tss.fa"
+                defaultFastaFile5 = "NC8_glc_1_tss.fa"
+                defaultFastaFile6 = "NC8_glc_2_tss.fa"
+                fasta(defaultFastaFile1,defaultFastaFile2,defaultFastaFile3,defaultFastaFile4,defaultFastaFile5,defaultFastaFile6)
+                print ("Files: genes1.txt, genes2.txt, genes3.txt, genes4.txt created")
+        if index == 8:
+                file = input[0]
+                file = "genes1.fa"
+                defaultMEMEFile = str (file)
+                MEME(defaultMEMEFile)
+                print ("Bestand is aangemaakt")
+                
+        
+        
+                
 def filter (defaultFilterFile):
   x = log2_fdr_filter.main(defaultFilterFile)
   print(x)
