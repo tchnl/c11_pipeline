5ad2b27bb5061c58728f33b43ce2a217471770d7
Marieke <mariekebeltman@hotmail.com>
2014-06-20 23:26:50 +0200

    change MEME

--- workflow/MEME.py
+++ workflow/MEME.py
@@ -7,9 +7,5 @@ import subprocess
 def main(file, index):
     
     MEME_input = str(file)
-    subprocess.call("cd /home/marieke/sharing/students/meme/bin ; ./meme /home/marieke/c11_pipeline/workflow/"+str(MEME_input)+" -dna -oc /home/marieke/c11_pipeline/output"+str(index)+"/ -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 35 -maxw 45 -revcomp", shell=True)
+    subprocess.call("cd <$path where meme is installed/bin> ; ./meme <$path to desktop>/c11_pipeline/workflow/"+str(MEME_input)+" -dna -oc /home/marieke/c11_pipeline/output"+str(index)+"/ -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 35 -maxw 45 -revcomp", shell=True)
     return True
-
-##    MEME_input = "/home/thierry/c11_pipeline/genes0.fa"
-##    subprocess.call("cd /sharing/students/meme/bin/ ; ./meme /home/thierry/c11_pipeline/genes0.fa -oc /home/thierry/output/ -nostatus -time 18000 -maxsize 60000 -mod zoops -nmotifs 3 -minw 35 -maxw 45 ", shell=True)
-
