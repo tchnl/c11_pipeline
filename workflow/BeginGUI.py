c3afdb2b8f4195a8812ffc18966757fd6120c3c4
Marieke <mariekebeltman@hotmail.com>
2014-06-20 14:44:09 +0200

    change

--- workflow/BeginGUI.py
+++ workflow/BeginGUI.py
@@ -13,12 +13,12 @@ class MyGUI:
         self.radio_var.set(1)
         self.__value = 1
         
-        self.label1 = tkinter.Label(self.top_frame, text = "Kies uit de volgende opties")
+        self.label1 = tkinter.Label(self.top_frame, text = "Choose one of the following options")
         self.label2 = tkinter.Label(self.top_frame, text = "\n")
 
-        self.rb1 = tkinter.Radiobutton(self.top_frame, text = "Alle modules zonder sort", variable=self.radio_var, value=1)
-        self.rb2 = tkinter.Radiobutton(self.top_frame, text = "Alle modules met sort", variable=self.radio_var, value=2)
-        self.rb3 = tkinter.Radiobutton(self.top_frame, text = "Kies losse modules", variable=self.radio_var, value=3)
+        self.rb1 = tkinter.Radiobutton(self.top_frame, text = "All modules without Sort", variable=self.radio_var, value=1)
+        self.rb2 = tkinter.Radiobutton(self.top_frame, text = "All modules include Sort", variable=self.radio_var, value=2)
+        self.rb3 = tkinter.Radiobutton(self.top_frame, text = "Select individual modules", variable=self.radio_var, value=3)
         
         self.my_button = tkinter.Button(self.main_window, text = "Ok", command=self.show_choice)
                 
@@ -57,13 +57,13 @@ class MyGUI2:
     def __init__(self):
         self.main_window = tkinter.Tk()
         
-        self.label = tkinter.Label(self.main_window, text = "Voer de volgende bestanden in:")
-        self.label2 = tkinter.Label(self.main_window, text = "Ratio's:")
-        self.label3 = tkinter.Label(self.main_window, text = "Genen:")
-        self.label4 = tkinter.Label(self.main_window, text = "Seq1:")
-        self.label5 = tkinter.Label(self.main_window, text = "Seq2:")
-        self.label6 = tkinter.Label(self.main_window, text = "Seq3:")
-        self.label7 = tkinter.Label(self.main_window, text = "Seq4:")
+        self.label = tkinter.Label(self.main_window, text = "Insert the filenames for the required files below:")
+        self.label2 = tkinter.Label(self.main_window, text = "File with ratios:")
+        self.label3 = tkinter.Label(self.main_window, text = "File with orthologues:")
+        self.label4 = tkinter.Label(self.main_window, text = "File with sequences from strain WCFS1 replicate 1:")
+        self.label5 = tkinter.Label(self.main_window, text = "File with sequences from strain WCFS1 replicate 2:")
+        self.label6 = tkinter.Label(self.main_window, text = "File with sequences from strain NC8 replicate 1:")
+        self.label7 = tkinter.Label(self.main_window, text = "File with sequences from strain NC8 replicate 2:")
         self.entry = tkinter.Entry(self.main_window, width = 40)
         self.entry2 = tkinter.Entry(self.main_window, width = 40)
         self.entry3 = tkinter.Entry(self.main_window, width = 40)
@@ -122,11 +122,11 @@ class MyGUI3:
         self.radio_var.set(4)
         self.__module = 4
 
-        self.label = tkinter.Label(self.main_window, text = "Kies uit 1 van de volgende modules:")
-        self.rb1 = tkinter.Radiobutton(self.main_window, text = "Filter", variable=self.radio_var, value=4)
-        self.rb2 = tkinter.Radiobutton(self.main_window, text = "Sort", variable=self.radio_var, value=5)
-        self.rb3 = tkinter.Radiobutton(self.main_window, text = "NZ/NC", variable=self.radio_var, value=6)
-        self.rb4 = tkinter.Radiobutton(self.main_window, text = "FASTA", variable=self.radio_var, value=7)
+        self.label = tkinter.Label(self.main_window, text = "Select one of the modules below:")
+        self.rb1 = tkinter.Radiobutton(self.main_window, text = "Filter ratios", variable=self.radio_var, value=4)
+        self.rb2 = tkinter.Radiobutton(self.main_window, text = "Sort ratios", variable=self.radio_var, value=5)
+        self.rb3 = tkinter.Radiobutton(self.main_window, text = "NZ/NC convert", variable=self.radio_var, value=6)
+        self.rb4 = tkinter.Radiobutton(self.main_window, text = "FASTA files maker", variable=self.radio_var, value=7)
         self.rb5 = tkinter.Radiobutton(self.main_window, text = "MEME", variable=self.radio_var, value=8)
 
         self.my_button3 = tkinter.Button(self.main_window, text = "Ok", command= self.module)                 
@@ -163,8 +163,8 @@ class MyGUI4:
 
     def __init__(self):
         self.main_window = tkinter.Tk()
-        self.label = tkinter.Label(self.main_window, text = "Voer de volgende bestanden in:")
-        self.label2 = tkinter.Label(self.main_window, text = "Ratio's:")
+        self.label = tkinter.Label(self.main_window, text = "Insert the filename for the required file below:")
+        self.label2 = tkinter.Label(self.main_window, text = "File with ratios:")
         self.entry = tkinter.Entry(self.main_window, width = 40)
         self.my_button3 = tkinter.Button(self.main_window, text = "Ok", command= self.invoer)                 
 
@@ -188,8 +188,8 @@ class MyGUI5:
 
     def __init__(self):
         self.main_window = tkinter.Tk()
-        self.label = tkinter.Label(self.main_window, text = "Voer de volgende bestanden in:")
-        self.label2 = tkinter.Label(self.main_window, text = "(Gefilterde) ratio's:")
+        self.label = tkinter.Label(self.main_window, text = "Insert the filename for the required file below:")
+        self.label2 = tkinter.Label(self.main_window, text = "File with (Filtered) ratios:")
         self.entry = tkinter.Entry(self.main_window, width = 40)
         self.my_button3 = tkinter.Button(self.main_window, text = "Ok", command= self.invoer)                 
 
@@ -213,8 +213,8 @@ class MyGUI6:
 
     def __init__(self):
         self.main_window = tkinter.Tk()
-        self.label = tkinter.Label(self.main_window, text = "Voer de volgende bestanden in:")
-        self.label2 = tkinter.Label(self.main_window, text = "Genen:")
+        self.label = tkinter.Label(self.main_window, text = "Insert the filename for the required file below:")
+        self.label2 = tkinter.Label(self.main_window, text = "File with orthologues:")
         self.entry = tkinter.Entry(self.main_window, width = 40)
         self.my_button3 = tkinter.Button(self.main_window, text = "Ok", command= self.invoer)                 
 
@@ -238,13 +238,13 @@ class MyGUI7:
 
     def __init__(self):
         self.main_window = tkinter.Tk()
-        self.label = tkinter.Label(self.main_window, text = "Voer de volgende bestanden in:")
-        self.label2 = tkinter.Label(self.main_window, text = "Gefilterde ratio's:")
-        self.label3 = tkinter.Label(self.main_window, text = "Genen:")
-        self.label4 = tkinter.Label(self.main_window, text = "Seq1:")
-        self.label5 = tkinter.Label(self.main_window, text = "Seq2:")
-        self.label6 = tkinter.Label(self.main_window, text = "Seq3:")
-        self.label7 = tkinter.Label(self.main_window, text = "Seq4:")
+        self.label = tkinter.Label(self.main_window, text = "Insert the filenames for the required files below:")
+        self.label2 = tkinter.Label(self.main_window, text = "File with (Filtered/sorted) ratios:")
+        self.label3 = tkinter.Label(self.main_window, text = "File with orthologues:")
+        self.label4 = tkinter.Label(self.main_window, text = "File with sequences from strain WCFS1 replicate 1:")
+        self.label5 = tkinter.Label(self.main_window, text = "File with sequences from strain WCFS1 replicate 2:")
+        self.label6 = tkinter.Label(self.main_window, text = "File with sequences from strain NC8 replicate 1:")
+        self.label7 = tkinter.Label(self.main_window, text = "File with sequences from strain NC8 replicate 2:")
         self.entry = tkinter.Entry(self.main_window, width = 40)
         self.entry2 = tkinter.Entry(self.main_window, width = 40)
         self.entry3 = tkinter.Entry(self.main_window, width = 40)
@@ -294,8 +294,8 @@ class MyGUI8:
 
     def __init__(self):
         self.main_window = tkinter.Tk()
-        self.label = tkinter.Label(self.main_window, text = "Voer de volgende bestanden in:")
-        self.label2 = tkinter.Label(self.main_window, text = "Output van Fasta:")
+        self.label = tkinter.Label(self.main_window, text = "Insert the filename for the required file below:")
+        self.label2 = tkinter.Label(self.main_window, text = "Outputfile from FASTA files maker:")
         self.entry = tkinter.Entry(self.main_window, width = 40)
         self.my_button3 = tkinter.Button(self.main_window, text = "Ok", command= self.invoer)                 
 
